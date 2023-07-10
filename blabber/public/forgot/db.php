98cb6ac217e59f7663edb279dc3b5a5aeed4eb08
John Mathen Wierengo <75157855+SackMathen@users.noreply.github.com>
2023-07-10 10:43:20 -0500

    Update db.php

--- blabber/public/forgot/db.php
+++ blabber/public/forgot/db.php
@@ -1,6 +1,6 @@
 <?php
 //https://www.allphptricks.com/forgot-password-recovery-reset-using-php-and-mysql/
-$dbc = new mysqli("localhost","Mathen","SackMathen_0","eventdb");
+$dbc = new mysqli("localhost","user1","pass","somethingdb");
 if (mysqli_connect_errno()){
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 die();
